d19e0f26081a1be922d0697237e7572a19ec7249
Henry Hoyos <hdosurrego@gmail.com>
2020-03-19 22:40:54 -0400

    .get replaced with fetch

--- js/script.js
+++ js/script.js
@@ -2,33 +2,58 @@
 
 (function() {
 	const url = "http://api.openweathermap.org/data/2.5/weather?q=";
-	const apiKey = "APIKEY"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
+	const apiKey = "3f0c3ea5238af794e735921936079f2f"; // Replace "APIKEY" with your own API key; otherwise, your HTTP request will not work
 	const activities = {
-		teamIn: ['basketball','hockey','volleyball'],
-		teamOutWarm: ['softball/baseball','football/soccer','American football','rowing','tennis','volleyball','ultimate frisbee','rugby'],
-		teamOutCold: ['hockey'],
-		soloIn: ['rock climbing','swimming','ice skating'],
-		soloOutWarm: ['rowing','running','hiking','cycling','rock climbing'],
-		soloOutCold: ['snowshoeing','downhill skiing','cross-country skiing','ice skating']
-	}
+		teamIn: ["basketball", "hockey", "volleyball"],
+		teamOutWarm: [
+			"softball/baseball",
+			"football/soccer",
+			"American football",
+			"rowing",
+			"tennis",
+			"volleyball",
+			"ultimate frisbee",
+			"rugby"
+		],
+		teamOutCold: ["hockey"],
+		soloIn: ["rock climbing", "swimming", "ice skating"],
+		soloOutWarm: ["rowing", "running", "hiking", "cycling", "rock climbing"],
+		soloOutCold: [
+			"snowshoeing",
+			"downhill skiing",
+			"cross-country skiing",
+			"ice skating"
+		]
+	};
 	let state = {};
-	let category = 'all';
+	let category = "all";
 
 	// get weather data when user clicks Forecast button, then add temp & conditions to view
-	$('.forecast-button').click(function(e) {
+	$(".forecast-button").click(function(e) {
 		e.preventDefault();
-		const location = $('#location').val();
-		$('#location').val('');
-
+		const location = $("#location").val();
+		$("#location").val("");
+		/*
 		$.get(url + location + '&appid=' + apiKey).done(function(response) {
 			updateUISuccess(response);
 		}).fail(function() {
 			updateUIFailure();
 		});
+*/
+		fetch(url + location + "&appid=" + apiKey)
+			.then(function(response) {
+				return response.json();
+			})
+			.then(function(response) {
+				updateUISuccess(response);
+			})
+			.catch(function() {
+				updateUIFailure();
+			});
 	});
 
 	// update list of sports when user selects a different category (solo/team/all)
-	$('.options div').on('click', updateActivityList);
+	$(".options div").on("click", updateActivityList);
 
 	// handle ajax success
 	function updateUISuccess(response) {
@@ -38,13 +63,14 @@
 		const degFInt = Math.floor(degF);
 		state = {
 			condition: response.weather[0].main,
-			icon: "http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
+			icon:
+				"http://openweathermap.org/img/w/" + response.weather[0].icon + ".png",
 			degCInt: Math.floor(degCInt),
 			degFInt: Math.floor(degFInt),
 			city: response.name
 		};
 
-		const $into = $('.conditions')[0];
+		const $into = $(".conditions")[0];
 
 		ReactDOM.render(<Forecast {...state} />, $into);
 
@@ -52,67 +78,70 @@
 			return (
 				<div>
 					<p className="city">{props.city}</p>
-					<p>{props.degCInt}&#176; C / {props.degFInt}&#176; F <img src={props.icon} alt={props.condition} /></p>
+					<p>
+						{props.degCInt}&#176; C / {props.degFInt}&#176; F{" "}
+						<img src={props.icon} alt={props.condition} />
+					</p>
 				</div>
-			)
+			);
 		}
 
 		updateActivityList();
 	}
 
-	// handle selection of a new category (team/solo/all) 
+	// handle selection of a new category (team/solo/all)
 	function updateActivityList(event) {
-		if (event !== undefined && $(this).hasClass('selected')) {
+		if (event !== undefined && $(this).hasClass("selected")) {
 			// if the 'event' parameter is defined, then a tab has been clicked; if not, then this is the
 			//   default case and the view simply needs to be updated
 			// if the clicked tab has the class 'selected', then no need to change location of 'selected' class
 			//   or change the DOM
 			return true;
-		} else if (event !== undefined && !$(this).hasClass('selected')) {
+		} else if (event !== undefined && !$(this).hasClass("selected")) {
 			// if the 'event' parameter is defined, then a tab has been clicked
 			// if the clicked tab does not have the class 'selected', then location of 'selected' class must be added
 			//   to the clicked element and removed from its siblings
-			category = $(this).attr('id');
-			$('.options div').removeClass('selected');
-			$(this).addClass('selected');
-		} 
+			category = $(this).attr("id");
+			$(".options div").removeClass("selected");
+			$(this).addClass("selected");
+		}
 
 		state.activities = [];
 		if (state.condition === "Rain") {
-			updateState('In');
+			updateState("In");
 		} else if (state.condition === "Snow" || state.degFInt < 50) {
-			updateState('OutCold');
+			updateState("OutCold");
 		} else {
-			updateState('OutWarm');
+			updateState("OutWarm");
 		}
 
 		function updateState(type) {
 			if (category === "solo") {
-				state.activities.push(...activities['solo' + type]);
+				state.activities.push(...activities["solo" + type]);
 			} else if (category === "team") {
-				state.activities.push(...activities['team' + type]);
+				state.activities.push(...activities["team" + type]);
 			} else {
-				state.activities.push(...activities['solo' + type]);
-				state.activities.push(...activities['team' + type]);
+				state.activities.push(...activities["solo" + type]);
+				state.activities.push(...activities["team" + type]);
 			}
 		}
 
-		const $into = $('.activities')[0];
+		const $into = $(".activities")[0];
 
 		ReactDOM.render(<Activities {...state} />, $into);
 
 		function Activities(props) {
 			const activitiesList = props.activities.map(function(activity, index) {
-				return <li key={index}>{activity}</li>
+				return <li key={index}>{activity}</li>;
 			});
 			return (
 				<div>
 					<ul>{activitiesList}</ul>
 				</div>
-			)
+			);
 		}
 
-		$('.results').slideDown(300);
+		$(".results").slideDown(300);
 	}
 
 	// handle ajax failure
